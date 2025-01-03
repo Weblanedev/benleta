f02e0eeb00c13e0f7bed8fd84685ea2bcce72e46
JAYPEE <eyibrajohnpaul@gmail.com>
2025-01-03 15:22:18 +0100

    feat: privacy policy page

--- src/app/privacy/page.tsx
+++ src/app/privacy/page.tsx
@@ -1,41 +1,173 @@
-'use client';
-
-import PrivacyView from '@/components/PrivacyView';
-import TermsView from '@/components/TermsView';
-// import { Metadata } from 'next';
-import Image from 'next/image';
-import React from 'react';
-
-// export const metadata:Metadata = {
-//   title: 'Privacy',
-// }
+"use client";
+import React from "react";
 
 const page = () => {
-  const [active, setactive] = React.useState<boolean>(true);
-
   return (
-    <div className='w-full bg-white'>
-      <div className='px-5 mx-auto md:w-[80%] lg:w-[70%] my-12 md:my-32'>
-        <div className='flex flex-row gap-x-4 mb-5'>
-          <button className={`${active === false ? 'text-[#132D46]' : 'text-[#099971] border-2 border-[#099971]'}  py-2 px-4 flex flex-row items-center gap-x-2 rounded-full`} onClick={()=> setactive(true)}>
-          {active === true && <Image src={'/shieldTick.svg'} width={100} height={100} className='h-5 w-5' alt='pic.png' /> }
-            <p className=''>
-              Privacy Policy
-            </p>
-          </button>
-          <button className={`${active === true ? 'text-[#132D46]' : 'text-[#099971] border-2 border-[#099971]'}  py-2 px-4 flex flex-row items-center gap-x-2 rounded-full`} onClick={()=> setactive(false)}>
-          {active === false && <Image src={'/starTerms.svg'} width={100} height={100} className='h-5 w-5' alt='pic.png' /> }
-            <p className=''>
-              Terms and Conditions
-            </p>
+    <main className="px-4 md:px-12 xl:px-16 pt-12 md:pt-20 text-black">
+      <h1 className="text-4xl md:text-7xl text-center font-bold mb-10">
+        Privacy Policy
+      </h1>
+      <p className="text-black font-general">
+        At Benleta, accessible from benleta.com, one of our main priorities is
+        the privacy of our visitors. This Privacy Policy document contains types
+        of information that is collected and recorded by Benleta and how we use
+        it.
+        <br />
+        <br />
+        If you have additional questions or require more information about our
+        Privacy Policy, do not hesitate to contact us at support@benleta.com.
+        <br />
+        <br />
+        This Privacy Policy applies only to our online activities and is valid
+        for visitors to our website with regard to the information that they
+        shared and/or collected in Benleta. This policy is not applicable to any
+        information collected offline or via channels other than this website.
+      </p>
 
-          </button>
-        </div>
+      <p className="text-2xl md:text-4xl font-semibold mt-4 mb-5 ">Consent</p>
+      <p className="text-black font-general">
+        By using our website, you hereby consent to our Privacy Policy and agree
+        to its terms.
+      </p>
 
-        {active ? <PrivacyView /> : <TermsView />}
-      </div>
-    </div>
+      <p className="text-xl md:text-3xl font-semibold mt-4 mb-5 ">
+        Information We Collect
+      </p>
+      <p className="text-black font-general">
+        The personal information that you are asked to provide, and the reasons
+        why you are asked to provide it, will be made clear to you at the point
+        we ask you to provide your personal information.
+        <br />
+        <br />
+        If you contact us directly, we may receive additional information about
+        you such as your name, email address, phone number, the contents of the
+        message and/or attachments you may send us, and any other information
+        you may choose to provide.
+        <br />
+        <br />
+        When you register for an Account, we may ask for your contact
+        information, including items such as name, company name, address, email
+        address, and telephone number.
+      </p>
+
+      <p className="text-xl md:text-3xl font-semibold mt-4 mb-5 ">
+        How We Use Your Information
+      </p>
+      <p className="text-black font-general">
+        We use the information we collect in various ways, including to:
+        <br />
+        <br />
+        <ul className="list-disc pl-4 font-general">
+          <li>Provide, operate, and maintain our website</li>
+          <li>Improve, personalize, and expand our website</li>
+          <li>Understand and analyze how you use our website</li>
+          <li>Develop new products, services, features, and functionality</li>
+          <li>
+            Communicate with you, either directly or through one of our
+            partners, including for customer service, to provide you with
+            updates and other information relating to the website, and for
+            marketing and promotional purposes
+          </li>
+          <li>Send you emails</li>
+          <li>Find and prevent fraud</li>
+        </ul>
+      </p>
+
+      <p className="text-xl md:text-3xl font-semibold mt-4 mb-5 ">Log Files</p>
+      <p className="text-black font-general">
+        Benleta follows a standard procedure of using log files. These files log
+        visitors when they visit websites. All hosting companies do this as a
+        part of hosting services' analytics. The information collected by log
+        files includes internet protocol (IP) addresses, browser type, Internet
+        Service Provider (ISP), date and time stamp, referring/exit pages, and
+        possibly the number of clicks. These are not linked to any information
+        that is personally identifiable. The purpose of the information is for
+        analyzing trends, administering the site, tracking users' movement on
+        the website, and gathering demographic information.
+      </p>
+      <p className="text-xl md:text-3xl font-semibold mt-4 mb-5 ">
+        Cookies and Web Beacons
+      </p>
+      <p className="text-black font-general">
+        Like many websites, Benleta uses cookies to enhance user experience.
+        These cookies store information about visitors’ preferences and the
+        pages accessed or visited. This data helps us tailor website content to
+        improve your experience.
+      </p>
+
+      <p className="text-xl md:text-3xl font-semibold mt-4 mb-5 ">
+        Advertising Partners Privacy Policies
+      </p>
+      <p className="text-black font-general">
+        Some of our advertising partners may use cookies, JavaScript, or Web
+        Beacons to measure the effectiveness of their ad campaigns. Benleta does
+        not have access to or control over these technologies used by
+        third-party advertisers. We encourage you to review their Privacy
+        Policies for more details.
+      </p>
+
+      <p className="text-xl md:text-3xl font-semibold mt-4 mb-5 ">
+        Third-Party Privacy Policies
+      </p>
+      <p className="text-black font-general">
+        Benleta's Privacy Policy does not cover other advertisers or websites.
+        For details about how third-party services handle your data, we
+        recommend reviewing their Privacy Policies.
+        <br />
+        <br />
+        You can also manage cookies through your browser settings. Detailed
+        instructions can be found on your browser’s website.
+      </p>
+      <p className="text-xl md:text-3xl font-semibold mt-4 mb-5 ">
+        Cryptocurrency and EU Compliance
+      </p>
+      <p className="text-black font-general">
+        Benleta ensures compliance with the European Union's cryptocurrency
+        regulations, including the Markets in Crypto-Assets Regulation (MiCA).
+        MiCA establishes clear guidelines for the issuance and trading of
+        digital assets, focusing on transparency and consumer protection.
+        <br />
+        <br />
+        We ensure that all crypto transactions on our platform comply with
+        anti-money laundering (AML) and know-your-customer (KYC) requirements.
+        Users can confidently transact, knowing that we adhere to EU standards
+        for financial integrity.
+      </p>
+
+      <p className="text-xl md:text-3xl font-semibold mt-4 mb-5 ">
+        Children’s Information
+      </p>
+      <p className="text-black font-general">
+        Benleta does not knowingly collect personal information from children
+        under 13. If you believe your child has shared such information with us,
+        contact us immediately, and we will take steps to remove it from our
+        records.
+      </p>
+
+      <p className="text-xl md:text-3xl font-semibold mt-4 mb-5 ">
+        Changes to This Privacy Policy
+      </p>
+      <p className="text-black font-general">
+        Benleta may update this Privacy Policy periodically. We encourage you to
+        review this page for updates. Any changes take effect immediately upon
+        posting.
+      </p>
+
+      <p className="text-xl md:text-3xl font-semibold mt-4 mb-5 ">
+        Changes to This Privacy Policy
+      </p>
+      <p className="text-black font-general">
+        We may update our Privacy Policy from time to time. Thus, we advise you
+        to review this page periodically for any changes. We will notify you of
+        any changes by posting the new Privacy Policy on this page. These
+        changes are effective immediately, after they are posted on this page.
+      </p>
+      <p className="text-xl md:text-3xl font-semibold mt-4 mb-5">Contact Us</p>
+      <p className="text-black font-general">
+        If you have any questions or suggestions about our Privacy Policy, do
+        not hesitate to contact us.{" "}
+      </p>
+    </main>
   );
 };
-
 export default page;
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,9 +1,9 @@
-'use client';
+"use client";
 
-import React from 'react';
-import Link from 'next/link';
-import Image from 'next/image';
-import { checkRoute } from '@/utils/checkRoute';
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { checkRoute } from "@/utils/checkRoute";
 
 export default function Navigation() {
   const [click, setClick] = React.useState(false);
@@ -11,35 +11,36 @@ export default function Navigation() {
   const closeMobileMenu = () => setClick(false);
 
   const deskLinks = [
-    { name: 'About Us', url: '/about', svg: '/shop.svg' },
-    { name: 'Privacy Policy', url: '/privacy', svg: '/shield.svg' },
-    { name: 'FAQs', url: '/faqs', svg: '/message-2.svg' },
+    { name: "About Us", url: "/about", svg: "/shop.svg" },
+    { name: "Privacy Policy", url: "/privacy", svg: "/shield.svg" },
+    { name: "FAQs", url: "/faqs", svg: "/message-2.svg" },
   ];
 
   return (
-    <div id='navbar' className='relative w-full '>
+    <div id="navbar" className="relative w-full ">
       <nav className={`flex items-center text-xl `}>
-        <div className='flex w-full flex-row items-center justify-between bg-transparent lg:px-12 p-5'>
+        <div className="flex w-full flex-row items-center justify-between bg-transparent lg:px-12 p-5">
           <Link
-            href='/'
-            className=' flex flex-row cursor-pointer text-3xl no-underline  w-fit'
-            onClick={closeMobileMenu}>
+            href="/"
+            className=" flex flex-row cursor-pointer text-3xl no-underline  w-fit"
+            onClick={closeMobileMenu}
+          >
             <Image
               width={150}
               height={80}
-              src={'/newtajalogo.png'}
-              alt='logo.png'
-              className='h-[40px] w-[75px] lg:w-[115px] lg:h-[55px]'
+              src={"/newtajalogo.png"}
+              alt="logo.png"
+              className="h-[40px] w-[75px] lg:w-[115px] lg:h-[55px]"
             />
           </Link>
-          <div className='block z-20 cursor-pointer' onClick={handleClick}>
+          <div className="block z-20 cursor-pointer" onClick={handleClick}>
             {click ? (
-              <div className='block md:hidden'>
-                <div className='h-[1.5px] w-6 rotate-45 bg-[#132D46]'></div>
-                <div className='ml-auto mt-1 h-[1.5px] w-6 -rotate-45 bg-[#132D46]'></div>
+              <div className="block md:hidden">
+                <div className="h-[1.5px] w-6 rotate-45 bg-[#132D46]"></div>
+                <div className="ml-auto mt-1 h-[1.5px] w-6 -rotate-45 bg-[#132D46]"></div>
               </div>
             ) : (
-              <div className='block md:hidden'>
+              <div className="block md:hidden">
                 <div className={`h-[2px] w-6 bg-[#132D46]`} />
                 <div className={`ml-auto mt-2 h-[2px] w-4 bg-[#132D46]`} />
               </div>
@@ -50,18 +51,20 @@ export default function Navigation() {
           <ul
             className={`z-[2] pl-0 md:hidden ${
               click
-                ? 'h-[100vh] top-0 fixed right-0 m-auto flex w-full list-none flex-col justify-center gap-[15px] bg-[#fff] text-center transition-all md:w-[60vw] md:justify-start'
-                : ' m-auto mr-8 hidden list-none flex-col justify-center gap-[15px] text-center transition-all md:w-[60vw] md:justify-start'
-            }`}>
+                ? "h-[100vh] top-0 fixed right-0 m-auto flex w-full list-none flex-col justify-center gap-[15px] bg-[#fff] text-center transition-all md:w-[60vw] md:justify-start"
+                : " m-auto mr-8 hidden list-none flex-col justify-center gap-[15px] text-center transition-all md:w-[60vw] md:justify-start"
+            }`}
+          >
             {deskLinks.map((item, index) => {
               return (
-                <li key={index} className=' md:hidden'>
+                <li key={index} className=" md:hidden">
                   <Link
                     href={item.url}
                     onClick={() => {
                       closeMobileMenu();
                     }}
-                    className={`cursor-pointer font-mono text-lg uppercase text-[#132D46]`}>
+                    className={`cursor-pointer font-mono text-lg uppercase text-[#132D46]`}
+                  >
                     {item.name}
                   </Link>
                 </li>
@@ -72,9 +75,10 @@ export default function Navigation() {
           {/* Desktop view */}
           <div
             className={` hidden gap-3 ${
-              click ? 'hidden' : 'md:flex md:flex-row justify-between w-full'
-            }`}>
-            <div className='flex flex-row gap-x-7 mx-auto  items-center my-auto px-4 py-[10px] '>
+              click ? "hidden" : "md:flex md:flex-row justify-between w-full"
+            }`}
+          >
+            <div className="flex flex-row gap-x-7 mx-auto  items-center my-auto px-4 py-[10px] ">
               {deskLinks.splice(0, 3).map((item, index) => {
                 return (
                   <Link
@@ -82,18 +86,10 @@ export default function Navigation() {
                     key={index}
                     className={`text-[16px] flex gap-2 ${
                       checkRoute(item.url)
-                        ? 'text-primary-800 hover:text-primary-700 py-[5px] font-bold underline transition-shadow'
-                        : 'text-primary-800 hover:text-primary-700 hover:underline'
-                    }`}>
-                    <Image
-                      alt='link.svg'
-                      width={100}
-                      height={100}
-                      src={item.svg}
-                      className={`w-5 h-5 mt-1  ${
-                        !checkRoute(item.url) ? 'hidden' : ''
-                      }`}
-                    />
+                        ? "text-primary-800 hover:text-primary-700 py-[5px] font-bold underline transition-shadow"
+                        : "text-primary-800 hover:text-primary-700 hover:underline"
+                    }`}
+                  >
                     {item.name}
                   </Link>
                 );
@@ -101,8 +97,8 @@ export default function Navigation() {
             </div>
           </div>
           <div className="bg-primary-800 text-white rounded-full px-4 py-2 text-lg">
-              <button>Contact</button>
-            </div>
+            <button>Contact</button>
+          </div>
         </div>
       </nav>
     </div>
